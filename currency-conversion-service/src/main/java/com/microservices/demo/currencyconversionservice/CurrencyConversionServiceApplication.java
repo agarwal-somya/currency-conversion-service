0fd73d826211074c7ffa9543e95ab782847f8f23
Somya Agarwal <agarwal.somya251992@gmail.com>
2019-05-10 16:26:51 -0400

    Connecting two microservices

---
+++ currency-conversion-service/src/main/java/com/microservices/demo/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.demo.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients("com.microservices.demo.currencyconversionservice")
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/microservices/demo/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+	// method to talk to currency exchange
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
